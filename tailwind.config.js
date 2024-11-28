7729c4d3b1783d2dfc078fe9a7ffbab5669ebb16
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2024-11-28 11:32:39 +0500

    code update

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this path based on your project structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,22 +12,6 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      screens: {
-        sm: "428px",
-        // => @media (min-width: 428px) { ... }
-
-        md: "768px",
-        // => @media (min-width: 768px) { ... }
-
-        lg: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
-        xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
-
-        "2xl": "1536px",
-        // => @media (min-width: 1536px) { ... }
-      },
       fontFamily: {
         rye: ["Rye", "cursive"],
         "libre-bodoni": ["Libre Bodoni", "serif"],
